70897aeda136689dcc54e6f8a2ea130e6e49c1b7
soda <soda@gmail.com>
2023-07-21 00:56:49 +0800

    时区

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,13 +2,17 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
     use HasFactory;
-
+    public function getCreatedAtAttribute($value): Carbon
+    {
+        return Carbon::parse($value)->timezone(config('app.timezone'));
+    }
     protected $casts = [
         'created_at' => 'datetime:Y-m-d H:i:s',
     ];
